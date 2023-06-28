561cb6336dc6c7ef725f80e6003939cda8bb8fc9
maxmkay <90427988+maxmkay@users.noreply.github.com>
2023-06-28 12:56:24 -0600

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,160 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='d-flex flex-col h-screen'>
+      <main className='flex-shrink-0'>
+        <nav className='navbar navbar-light bg-white py-3'>
+          <div className='container px-5'>
+            <div className='flex items-center'>
+              <Image
+                src={'/logo.png'}
+                alt='logo'
+                className='w-16 h-16 rounded'
+                width='64'
+                height={64}
+              />
+              <Link href='/' className='navbar-brand text-primary'>
+                TopProp
+              </Link>
+            </div>
+
+            <button className='navbar-toggler' type='button'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse'>
+              <ul className='navbar-nav ms-auto mb-2 mb-lg-0 small'>
+                <li className='nav-item'>
+                  <Link href='/' className='nav-link'>
+                    Home
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link href='/resume' className='nav-link'>
+                    Resume
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link href='/projects' className='nav-link'>
+                    Projects
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link href='/contact' className='nav-link'>
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <header className='py-5'>
+          <div className='container px-5 pb-5'>
+            <div className='row align-items-center'>
+              <div className='col-xxl-5'>
+                <div className='text-center text-xxl-start'>
+                  <div className='badge bg-gradient-primary-to-secondary text-white mb-4'>
+                    <div className='text-uppercase'>
+                      Design &middot; Development &middot; Marketing
+                    </div>
+                  </div>
+                  <div className='fs-3 fw-light text-muted'>
+                    I can help your business to
+                  </div>
+                  <h1 className='display-3 fw-bolder mb-5'>
+                    <span className='text-gradient d-inline'>
+                      Get online and grow fast
+                    </span>
+                  </h1>
+                  <div className='d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3'>
+                    <Link
+                      href='/resume'
+                      className='btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder'
+                    >
+                      Resume
+                    </Link>
+                    <Link
+                      href='/projects'
+                      className='btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder'
+                    >
+                      Projects
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className='col-xxl-7'>
+                <div className='d-flex justify-content-center mt-5 mt-xxl-0'>
+                  <div className='profile bg-gradient-primary-to-secondary'>
+                    <Image
+                      src='/profile.png'
+                      alt='Profile'
+                      width={500}
+                      height={300}
+                    />
+                    {/* SVG dots were here but they were not given so I removed them */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <section className='bg-light py-5'>
+          <div className='container px-5'>
+            <div className='row justify-content-center'>
+              <div className='col-xxl-8'>
+                <div className='text-center my-5'>
+                  <h2 className='display-5 fw-bolder'>
+                    <span className='text-gradient d-inline'>About Me</span>
+                  </h2>
+                  <p className='lead fw-light mb-4'>
+                    My name is Start Bootstrap and I help brands grow.
+                  </p>
+                  <p className='text-muted'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Fugit dolorum itaque qui unde quisquam consequatur autem.
+                    Eveniet quasi nobis aliquid cumque officiis sed rem iure
+                    ipsa! Praesentium ratione atque dolorem?
+                  </p>
+                  <div className='d-flex justify-content-center fs-2 gap-4'>
+                    <a className='text-gradient' href='#'>
+                      <i className='bi bi-twitter'></i>
+                    </a>
+                    <a className='text-gradient' href='#'>
+                      <i className='bi bi-linkedin'></i>
+                    </a>
+                    <a className='text-gradient' href='#'>
+                      <i className='bi bi-github'></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className='bg-white py-4 mt-auto'>
+        <div className='container px-5'>
+          <div className='row align-items-center justify-content-between flex-col flex-sm-row'>
+            <div className='col-auto'>
+              <p className='small m-0'>Copyright &copy; Your Website 2023</p>
+            </div>
+            <div className='col-auto'>
+              <Link href='/privacy' className='small'>
+                Privacy
+              </Link>
+              <span className='mx-1'>&middot;</span>
+              <Link href='/terms' className='small'>
+                Terms
+              </Link>
+              <span className='mx-1'>&middot;</span>
+              <Link href='/contact' className='small'>
+                Contact
+              </Link>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </footer>
+    </div>
+  );
 }
